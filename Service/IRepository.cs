a2ad0a5a5413586d020ed5052d011479eb636b92
Owais Sohail <owais37@gmail.com>
2017-09-11 12:20:56 -0400

    .

---
+++ Service/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T GetById(object id);
+        T FindById(Func<T, bool> where);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Add(T entity);
+        void Attach(T entity);
+    }
+}
+
