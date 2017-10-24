a2fbfe29fca2e3f2585922504ceea7b55aeb65a3
Owais <aowais37@gmail.com>
2017-10-24 06:23:42 -0400

    .

---
+++ WebApplication1/Scripts/MyApp.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="../Scripts/angular.js" />
+
+
+var app = angular
+            .module("Demo", ["ngRoute"])
+            .config(function ($routeProvider) {
+                $routeProvider
+                    .when("/home", {
+                        templateUrl: "../Template/Home.html",
+                        controller: "homeController"
+                    })
+                    .when("/courses", {
+                        templateUrl: "Templates/courses.html",
+                        controller: "coursesController"
+                    })
+                    .when("/students", {
+                        templateUrl: "Templates/students.html",
+                        controller: "studentsController"
+                    })
+            })
+            .controller("homeController", function ($scope) {
+                $scope.message = "Home Page";
+            })
+            .controller("coursesController", function ($scope) {
+                $scope.courses = ["C#", "VB.NET", "ASP.NET", "SQL Server"];
+            })
+             .controller("studentsController", function ($scope, $http) {
+                 $http.get("StudentService.asmx/GetAllStudents")
+                                        .then(function (response) {
+                                            $scope.students = response.data;
+                                        })
+             })
